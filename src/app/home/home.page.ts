391ce3bda9a5b939766d3a38750a26d8ad9b68e2
Reimar-Coelho <reimargamer@proton.me>
2024-04-25 08:49:29 -0300

    ionic range

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RangeCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,17 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  valorRange = 0;
+  valorConta = '';
+  valorGorjeta = 0;
+  valorTotal = 0;
 
+  calcularGorjeta() {
+    this.valorGorjeta = (this.valorRange * 0.01)* parseFloat(this.valorConta);
+    this.valorTotal = parseFloat(this.valorConta)+this.valorGorjeta;
+  }
+  onIonChange(ev:RangeCustomEvent) {
+    this.valorRange = parseInt(ev.detail.value.toString());
+    this.calcularGorjeta();
+  }
 }
