5d40224770234ee813933458351b3cd8e561cbfa
Reimar-Coelho <reimargamer@proton.me>
2024-05-02 08:26:02 -0300

    conversor de unidades de medida

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -22,4 +22,38 @@ export class HomePage {
     this.valorRange = parseInt(ev.detail.value.toString());
     this.calcularGorjeta();
   }
+
+  unidadeSelecionada = ''
+  unidadeSelecionada2 = ''
+  valorConversao = 0
+
+  converter(){
+    if(this.unidadeSelecionada == 'km' && this.unidadeSelecionada2 == 'milhas'){
+      this.valorConversao = this.valorRange * 0.621371
+    }
+    else if(this.unidadeSelecionada == 'km' && this.unidadeSelecionada2 == 'pes'){
+      this.valorConversao = this.valorRange * 3280.84
+    }
+    else if(this.unidadeSelecionada == 'km' && this.unidadeSelecionada2 == 'jardas'){
+      this.valorConversao = this.valorRange * 1093.61
+    }
+    else if(this.unidadeSelecionada == 'm' && this.unidadeSelecionada2 == 'milhas'){
+      this.valorConversao = this.valorRange * 0.000621371
+    }
+    else if(this.unidadeSelecionada == 'm' && this.unidadeSelecionada2 == 'pes'){
+      this.valorConversao = this.valorRange * 3.28084
+    }
+    else if(this.unidadeSelecionada == 'm' && this.unidadeSelecionada2 == 'jardas'){
+      this.valorConversao = this.valorRange * 1.09361
+    }
+    else if(this.unidadeSelecionada == 'cm' && this.unidadeSelecionada2 == 'milhas'){
+      this.valorConversao = this.valorRange * 0.0000062137
+    }
+    else if(this.unidadeSelecionada == 'cm' && this.unidadeSelecionada2 == 'pes'){
+      this.valorConversao = this.valorRange * 0.0328084
+    }
+    else if(this.unidadeSelecionada == 'cm' && this.unidadeSelecionada2 == 'jardas'){
+      this.valorConversao = this.valorRange * 0.0109361
+    }
+  }
 }
